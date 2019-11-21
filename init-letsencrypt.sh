d1e69ef4349ea90f56093647b1a0905afa6548f7
daniel <daniel.z@shinehub.com.au>
2019-11-22 09:47:31 +1100

    update init file

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(monitor.shinehub.com.au www.monitor.shinehub.com.au)
+domains=(monitor.shinehub.com.au)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="danny.s@shinehub.com.au" # Adding a valid address is strongly recommended
