c4390339213c9f2350d73b91e521491738845590
Farrukh Ehsan <farrukh.e@shinehub.com.au>
2019-11-29 11:55:56 +0530

    Charts updated with being minimalistic in accordance with the existing UI

--- src/components/common/Chart.js
+++ src/components/common/Chart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import HighStock from 'react-highcharts/ReactHighstock.src';
+import HighStock from 'react-highcharts';
 
 
 class Chart extends Component {
@@ -15,33 +15,51 @@ class Chart extends Component {
     const newTime=time.map(time=>([time,parseInt(time.split(":")[1])]));
     console.log('newTime',newTime);
   const config = {
-      rangeSelector: {
-        lang:{
-          rangeSelectorZoom:''
-        },
-        selected: 1,
-        inputEnabled:false,
-        buttonTheme:{
-          visibility:'hidden'
-        },
-        labelStyle:{
-          visibility:'hidden'
+      // rangeSelector: {
+      //   enabled:false,
+      //   lang:{
+      //     rangeSelectorZoom:''
+      //   },
+      //   xAxis:{
+      //     enabled:false
+      //   },
+      //   selected: 1,
+      //   inputEnabled:false,
+      //   buttonTheme:{
+      //     visibility:'hidden'
+      //   },
+      //   labelStyle:{
+      //     visibility:'hidden'
+      //   }
+      // },
+      title: {
+        text: ''
+      },
+      legend:{
+        enabled:false
+      },
+      plotOptions:
+      {
+        line:{
+          marker:{
+            enabled:false
+          }
         }
       },
-      title: {
-        // text: ''
-      },
       xAxis:[{
-        type:'category',
-        categories:newTime,
+        // type:'category',
+        categories:time,
         labels:{
           enabled:true,
           format: "{value}"
         }
       }],
-      series: [{
-        data:newTime,
-      },{
+      series: [
+      //   {
+      //   data:newTime,
+      // }
+      // ,
+      {
         name: 'Available Power',
         data: availablePower,
         color:"#25A8A8",
