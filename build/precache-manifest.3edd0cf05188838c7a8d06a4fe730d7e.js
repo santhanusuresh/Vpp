8cdc90a263f82f48d55c2f6fec3623d472c4a0d8
danielz <danielz@shinehub.com.au>
2020-01-04 22:59:13 +1100

    debug

---
+++ build/precache-manifest.3edd0cf05188838c7a8d06a4fe730d7e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9388b82d44025e4f0564f7741d677794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "350b7a8f31bcdf37ac4e",
+    "url": "/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "26e7716be4861532c0a4",
+    "url": "/static/css/main.663af042.chunk.css"
+  },
+  {
+    "revision": "350b7a8f31bcdf37ac4e",
+    "url": "/static/js/2.e010a7d7.chunk.js"
+  },
+  {
+    "revision": "26e7716be4861532c0a4",
+    "url": "/static/js/main.d9becf08.chunk.js"
+  },
+  {
+    "revision": "45e0de957477d8bfc52b",
+    "url": "/static/js/runtime~main.aa4d6f45.js"
+  },
+  {
+    "revision": "c777eefd1941153363526a4d69cde20e",
+    "url": "/static/media/Avatar.c777eefd.svg"
+  },
+  {
+    "revision": "00b9403346bd5bb8a13ca2e85720196a",
+    "url": "/static/media/Calendar.00b94033.svg"
+  },
+  {
+    "revision": "640c207ce5fa6169a2b02c73797353b1",
+    "url": "/static/media/Gotham-Rounded-Bold.640c207c.woff"
+  },
+  {
+    "revision": "6a4f95c7904e0d0c0f64a60a6ab4e386",
+    "url": "/static/media/Gotham-Rounded-Light.6a4f95c7.woff"
+  },
+  {
+    "revision": "29c9cff4805f2c09a2b62133f2f8a4e4",
+    "url": "/static/media/Gotham-Rounded-Medium.29c9cff4.woff"
+  },
+  {
+    "revision": "386b41d8893adf8d6f18692253c6d4d1",
+    "url": "/static/media/Shape.386b41d8.svg"
+  },
+  {
+    "revision": "07a5723295644be79473ff6dd554463b",
+    "url": "/static/media/avatar-icon.07a57232.png"
+  },
+  {
+    "revision": "df50409b5d28d76e52c35c38c8505762",
+    "url": "/static/media/cog-wheel.df50409b.svg"
+  },
+  {
+    "revision": "be206108ed33c60d69ee19c2dac67b0d",
+    "url": "/static/media/four-rectangles.be206108.svg"
+  },
+  {
+    "revision": "e9c5f666e7ce1c488c04937d4956bed0",
+    "url": "/static/media/icon-analytics.e9c5f666.svg"
+  },
+  {
+    "revision": "976a89a08fe0aef0e1a68d44865f8bf9",
+    "url": "/static/media/map.976a89a0.svg"
+  },
+  {
+    "revision": "c089646c939a43aa0b7c6b7a095b41b9",
+    "url": "/static/media/menu-icon.c089646c.svg"
+  },
+  {
+    "revision": "6b9f82db938d16be348201a387136c09",
+    "url": "/static/media/shinehub-logo.6b9f82db.svg"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/static/media/spinner.0d0345f6.gif"
+  },
+  {
+    "revision": "668a2a221bad4ade53fc5c877f6599de",
+    "url": "/static/media/stock-market-arrow-up.668a2a22.svg"
+  },
+  {
+    "revision": "a875d9dd37784bdc0bd2a50c2a39e895",
+    "url": "/static/media/stock-market-arrow.a875d9dd.svg"
+  }
+]);
